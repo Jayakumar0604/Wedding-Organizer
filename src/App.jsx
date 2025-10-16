6a00d0171bbf7caaa6959910b263a335b5373b7a
jayakumar0604 <jayakumarcseo421@gmail.com>
2025-10-16 16:51:36 +0530

    Fix Import section on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Mainlayout from "../../../Tasks/login-signup/src/Components/Mainlayout";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
 import Gallery from "./Pages/Gallery";
 import Testimonials from "./Pages/Testimonials";
 import Contact from "./Pages/Contact";
+import MainLayouts from "./Layouts/MainLayouts";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Mainlayout />}>
+        <Route path="/" element={<MainLayouts />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="services" element={<Services />} />
